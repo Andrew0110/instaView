dd802b18c3e37641236f22e0baaa3e896e657530
Andrew <r.andrew0110@gmail.com>
2015-09-03 17:56:29 +0300

    Initial Commit

---
+++ InstaView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaView
+//
+//  Created by Andrew on 03.09.15.
+//  Copyright (c) 2015 obodev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InstaView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaView
+//
+//  Created by Andrew on 03.09.15.
+//  Copyright (c) 2015 obodev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
