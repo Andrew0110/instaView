3c9fe0d9fd64178ae0323209b9181f2b0ef7e2ed
Andrew <r.andrew0110@gmail.com>
2015-10-07 22:48:10 +0300

    Created login screen

---
+++ InstaView/InstagramLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstagramLoginViewController.h
+//  InstaView
+//
+//  Created by Andrew on 06.10.15.
+//  Copyright © 2015 obodev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstagramLoginViewController : UIViewController
+
+@end
